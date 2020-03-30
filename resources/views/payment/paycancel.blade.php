832f469192d2fd0d6803f92078da1843659a1543
simtj-tdi <simtj@nsmg21.com>
2020-03-30 11:13:07 +0900

    결제페이지 수정
    결제페이지 수정

--- resources/views/payment/paycancel.blade.php
+++ resources/views/payment/paycancel.blade.php
@@ -1,21 +1,9 @@
 @extends('layouts.app')
+@prepend('scripts')
 
+@endprepend
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">결제 취소</div>
-
-                    <div class="card-body">
-
-                        <div class="alert alert-danger" role="alert">
-                            사용자의 의해서 취소 되었습니다.
-                        </div>
-                        <a class="btn btn-primary" href="javascript:self.close();" role="button">창 닫기</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <script>
+        javascript:self.close();
+    </script>
 @endsection
--- resources/views/payment/payreturn.blade.php
+++ resources/views/payment/payreturn.blade.php
@@ -1,21 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">결제 취소</div>
-
-                    <div class="card-body">
-
-                        <div class="alert alert-success" role="alert">
-                            정상적으로 결제가 완료 되었습니다.
-                        </div>
-                        <a class="btn btn-primary" href="javascript:self.close();opener.location.reload(true);" role="button">창 닫기</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <script>
+        opener.location.reload(true);
+        javascript:self.close();
+    </script>
 @endsection
